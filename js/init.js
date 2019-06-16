1f09203e5fc02a952d29e1d17a9f7cb172b53a40
MarisaConway <marisaconway0111@gmail.com>
2019-06-15 22:46:28 -0400

    start of project

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+
+    // modal
+    $(document).ready(function(){
+      $('.modal').modal();
+    });
+})(jQuery); // end of jQuery name space
+
+
